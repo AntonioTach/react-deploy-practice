c9dcde95317f35c12c5bd90c0edc4428f9ed5b80
Antonio Tach <antach.vihe@gmail.com>
2022-10-16 19:50:57 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "Descripcion de primer tarea",
+  },
+  {
+    id: 1,
+    title: "Mi Segunda tarea",
+    description: "Descripcion de segunda tarea",
+  },
+  {
+    id: 2,
+    title: "Mi Tercera tarea",
+    description: "Descripcion de Tercera tarea",
+  },
+];
